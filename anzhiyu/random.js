910873b5d6b9d21601bdc126c2a36cd2acfdee01
Meilunova <3330876408@qq.com>
2025-08-21 18:50:05 +0800

    Site updated: 2025-08-21 18:50:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/21/Hexo博客搭建完整指南：从零开始打造你的个人博客/","2025/07/21/hexo-anzhiyu-theme-tutorial/","2025/01/20/anzhiyu-theme-advanced-customization/"];function toRandomPost(){
+var posts=["2025/07/25/anzhiyu-theme-advanced-customization/","2025/07/21/Hexo博客搭建完整指南：从零开始打造你的个人博客/","2025/07/21/hexo-anzhiyu-theme-tutorial/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
